c4ea547ac69236afc8a3bf1cab6ab606ee28b14e
김수미 서비스개발팀 <sumikim@sk.com>
2024-02-14 11:12:25 +0900

    :sparkles: 4장 고차 함수 : 실습 코드 추가

--- src/main/kotlin/ch04/composedFunction/GcdPowerOfTwo.kt
+++ src/main/kotlin/ch04/composedFunction/GcdPowerOfTwo.kt
@@ -1,6 +1,13 @@
 package ch04.composedFunction
 
 fun main(args: Array<String>) {
+    infix fun <F, G, R> ((F) -> R).compose(g: (G) -> F): (G) -> R {
+        return { gInput: G -> this(g(gInput)) }
+    }
+
+    fun <P1, P2, R> ((P1, P2) -> R).curried():
+                (P1) -> (P2) -> R = { p1: P1 -> { p2: P2 -> this(p1, p2) } }
+
     tailrec fun gcd(m: Int, n: Int): Int = when (n) {
         0 -> m
         else -> gcd(n, m % n)
@@ -17,7 +24,18 @@ fun main(args: Array<String>) {
     println(gcdPowerOfTwo(25, 5))
 
     // 잘못된 composedGcdPowerOfTwo
+    val curriedGcd1 = :: gcd.curried()
+    // val curriedGcd1 = { m: Int, n: Int -> gcd(m, n) }.curried()
+        // gcd.curried() 를 하면 함수의 반환값인 Int 타입를 얻지만
+        // :: gcd.curried() 를 하면 gcd 함수의 원형인 ((Int, Int) -> Int) 타입을 얻는다.
+        // :: gcd 는 { (m: Int, n: Int) -> gcd(m, n) } 과 동일
+    val composedGcdPowerOfTwo1 = curriedGcd1 compose powerOfTwo
+    println(composedGcdPowerOfTwo1(25)(5))
 
     // 올바른 결과를 return 하는 composedGcdPowerOfTwo
-
+    val curriedGcd2 = { m: Int, n: Int -> gcd(m, powerOfTwo(n)) }.curried()
+        // 모양이 이상하죠? 여러개의 매개변수에 동일한 함수를 적용해야 할 때 함수 합성을 사용하는 것은 적합하지 않다.
+        // gcdPowerOfTwo 처럼 일반적인 고차 함수로 연결하는 것이 좋다.
+    val composedGcdPowerOfTwo2 = curriedGcd2 compose powerOfTwo
+    println(composedGcdPowerOfTwo2(25)(5))
 }
---
+++ src/main/kotlin/ch04/composedFunction/ZipWith.kt
@@ -0,0 +1,38 @@
+package ch04.composedFunction
+
+fun main(args: Array<String>) {
+    fun <T> List<T>.head() = first()
+    fun <T> List<T>.tail() = drop(1)
+
+    tailrec fun <P1, P2, R> zipWith(func: (P1, P2) -> R, list1: List<P1>, list2: List<P2>, acc: List<R> = listOf()): List<R> = when {
+        list1.isEmpty() || list2.isEmpty() -> acc
+        else -> {
+            val zipList = acc + listOf(func(list1.head(), list2.head()))
+            zipWith(func, list1.tail(), list2.tail(), zipList)
+        }
+    }
+
+    val list1 = listOf(6, 3, 2, 1, 4)
+    val list2 = listOf(7, 4, 2, 6, 3)
+    val add = { p1: Int, p2: Int -> p1 + p2 }
+
+    val result1 = zipWith(add, list1, list2)
+    println(result1)
+
+    // 연습문제 4-7
+    // 리스트의 값을 조건 함수에 적용했을 때 결괏값이 참인 리스트를 반환하는 takeWhile 함수를 꼬리 재귀로 작성하라.
+    tailrec fun <P1> onlyTrue(func: (P1) -> Boolean, list1: List<P1>, acc: List<P1> = listOf()): List<P1> = when {
+        list1.isEmpty() -> acc
+        else -> {
+            var zipList = acc
+            if (func(list1.head())) { zipList = zipList + list1.head() }
+            onlyTrue(func, list1.tail(), zipList)
+        }
+    }
+
+    val myList = listOf(6, 3, 2, 1, 4)
+    val above = { p1: Int -> p1 > 3 }
+
+    val myResult = onlyTrue(above, myList)
+    println(myResult)
+}
