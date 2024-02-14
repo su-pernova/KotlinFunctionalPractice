65fb21f5be88ac6da18ef4870b03dcf7dcc22503
김수미 서비스개발팀 <sumikim@sk.com>
2024-02-14 17:57:12 +0900

    :sparkles: 4장 고차 함수 : 연습문제 4-8 풀이 추가

--- src/main/kotlin/ch04/composedFunction/ZipWith.kt
+++ src/main/kotlin/ch04/composedFunction/ZipWith.kt
@@ -21,18 +21,35 @@ fun main(args: Array<String>) {
 
     // 연습문제 4-7
     // 리스트의 값을 조건 함수에 적용했을 때 결괏값이 참인 리스트를 반환하는 takeWhile 함수를 꼬리 재귀로 작성하라.
-    tailrec fun <P1> onlyTrue(func: (P1) -> Boolean, list1: List<P1>, acc: List<P1> = listOf()): List<P1> = when {
+    tailrec fun <P1> takeWhile(func: (P1) -> Boolean, list1: List<P1>, acc: List<P1> = listOf()): List<P1> = when {
         list1.isEmpty() -> acc
         else -> {
             var zipList = acc
             if (func(list1.head())) { zipList = zipList + list1.head() }
-            onlyTrue(func, list1.tail(), zipList)
+            takeWhile(func, list1.tail(), zipList)
         }
     }
 
     val myList = listOf(6, 3, 2, 1, 4)
-    val above = { p1: Int -> p1 > 3 }
-
-    val myResult = onlyTrue(above, myList)
+    val above = { p1: Int -> p1 < 3 }
+    val myResult = takeWhile(above, myList)
     println(myResult)
+
+    // 연습문제 4-8
+    // 무한대를 입력 받을 수 있는 takeWhile 을 꼬리 재귀로 작성해 보자.
+    tailrec fun <P1> takeWhileInfinite(func: (P1) -> Boolean, sequence1: Sequence<P1>, acc: List<P1> = listOf()): List<P1> = when {
+        !sequence1.iterator().hasNext() -> acc
+        else -> {
+            var zipList = acc
+            if (func(sequence1.first())) {
+                zipList = zipList + sequence1.first()
+                takeWhileInfinite(func, sequence1.drop(1), zipList)
+            }
+            else takeWhileInfinite(func, sequenceOf(), zipList)
+        }
+    }
+
+    val infiniteValue = generateSequence(1) { it + 1 }
+    val infiniteResult = takeWhileInfinite(above, infiniteValue)
+    println(infiniteResult)
 }
